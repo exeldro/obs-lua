5a6153a164409ee99f27fed135987894a9d60591
Exeldro <exelsor@hotmail.com>
2020-09-19 17:01:12 +0200

    add option to always show one

--- source-toggler.lua
+++ source-toggler.lua
@@ -1,8 +1,10 @@
 obs           = obslua
 gs            = nil
+always_show   = false
 function script_properties()
     local props = obs.obs_properties_create()
-    local p = obs.obs_properties_add_editable_list(props, "sources", "Scenes and Groups",obs.OBS_EDITABLE_LIST_TYPE_STRINGS,nil,nil)
+    obs.obs_properties_add_editable_list(props, "sources", "Scenes and Groups",obs.OBS_EDITABLE_LIST_TYPE_STRINGS,nil,nil)
+    obs.obs_properties_add_bool(props, "always_show", "Always show")
 	return props
 end
 
@@ -12,7 +14,7 @@ end
 
 function item_visible(calldata)
     local visible = obs.calldata_bool(calldata,"visible")
-    if not visible then
+    if not visible and not always_show then
         return
     end
     local item = obs.calldata_sceneitem(calldata,"item")
@@ -20,17 +22,26 @@ function item_visible(calldata)
     local sourceName = obs.obs_source_get_name(source)
     local scene = obs.obs_sceneitem_get_scene(item)
     local sceneitems = obs.obs_scene_enum_items(scene)
+    local found = false
     for i, sceneitem in ipairs(sceneitems) do
         local itemsource = obs.obs_sceneitem_get_source(sceneitem)
         local isn = obs.obs_source_get_name(itemsource)
-        if sourceName ~= isn then
-            obs.obs_sceneitem_set_visible(sceneitem, false)
+        if visible and sourceName ~= isn then
+            if obs.obs_sceneitem_visible(sceneitem) then
+                obs.obs_sceneitem_set_visible(sceneitem, false)
+            end
+        elseif not visible and sourceName ~= isn and obs.obs_sceneitem_visible(sceneitem) then
+            found = true
         end
+    end
+    if always_show and not visible and not found then
+        obs.obs_sceneitem_set_visible(item, true)
     end
     obs.sceneitem_list_release(sceneitems)
 end
 
 function script_update(settings)
+    always_show = obs.obs_data_get_bool(settings,"always_show")
     local sourceNames =  obs.obs_data_get_array(settings, "sources");
     local count = obs.obs_data_array_count(sourceNames);
     for i = 0,count do 
@@ -78,6 +89,7 @@ end
 
 function script_load(settings)
     gs = settings
+    always_show = obs.obs_data_get_bool(settings,"always_show")
     local sh = obs.obs_get_signal_handler()
     obs.signal_handler_connect(sh, "source_load", loaded)
 end
