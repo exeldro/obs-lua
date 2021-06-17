62832183f0a2be2a2265bbb4fa77c76b9f6c14c8
Exeldro <exelsor@hotmail.com>
2021-06-17 11:06:58 +0200

    media-hide.lua add include/exclude list

--- media-hide.lua
+++ media-hide.lua
@@ -1,8 +1,8 @@
 obs           = obslua
+gs            = nil
 
 function source_enable(source, enable)
     local sourcename = obs.obs_source_get_name(source)
-
     local scenes = obs.obs_frontend_get_scenes()
     if scenes ~= nil then
         for _, scenesource in ipairs(scenes) do
@@ -36,6 +36,8 @@ end
 
 function script_properties()
 	local props = obs.obs_properties_create()
+    obs.obs_properties_add_bool(props, "exclude", "Exclude")
+    obs.obs_properties_add_editable_list(props, "sources", "Sources",obs.OBS_EDITABLE_LIST_TYPE_STRINGS,nil,nil)
     return props;
 end
 
@@ -43,37 +45,56 @@ function script_description()
 	return "Hide media sources when ended"
 end
 
-function script_update(settings)
+function media_ended(cd)
+    local source = obs.calldata_source(cd, "source")
+    source_enable(source, false);
+end
 
+function update_source_media_ended(source, sourceNames, exclude)
+    local sh = obs.obs_source_get_signal_handler(source);
+    obs.signal_handler_disconnect(sh,"media_ended",media_ended)
+    local sn = obs.obs_source_get_name(source)
+    local found = false
+    local count = obs.obs_data_array_count(sourceNames);
+    for i = 0,count do 
+        local item = obs.obs_data_array_item(sourceNames, i);
+        if obs.obs_data_get_string(item, "value") == sn then
+            found = true
+        end
+    end
+    if (found == true and exclude == false) or (found == false and exclude == true) then
+        obs.signal_handler_connect(sh,"media_ended",media_ended)
+    end
+end
+
+function script_update(settings)
+    local exclude = obs.obs_data_get_bool(settings,"exclude")
+    local sourceNames =  obs.obs_data_get_array(settings, "sources");
+    local sources = obs.obs_enum_sources()
+    if sources ~= nil then
+        for _, source in ipairs(sources) do
+            update_source_media_ended(source, sourceNames, exclude)
+        end
+        obs.source_list_release(sources)
+    end
 end
 
 function script_defaults(settings)
 
 end
 
-function media_ended(cd)
-    local source = obs.calldata_source(cd, "source")
-    source_enable(source, false);
-end
-
-function listen_media_ended(source) 
-    local sh = obs.obs_source_get_signal_handler(source);
-    obs.signal_handler_disconnect(sh,"media_ended",media_ended)
-    obs.signal_handler_connect(sh,"media_ended",media_ended)
-end
-
 function source_create(cd)
-    listen_media_ended(obs.calldata_source(cd, "source"))
+    if gs == nil then
+        return
+    end
+    local sourceNames = obs.obs_data_get_array(gs, "sources");
+    local exclude = obs.obs_data_get_bool(gs,"exclude")
+    local source = obs.calldata_source(cd, "source")
+    update_source_media_ended(source, sourceNames, exclude)
 end
 
 function script_load(settings)
-    local sources = obs.obs_enum_sources()
-    if sources ~= nil then
-        for _, source in ipairs(sources) do
-            listen_media_ended(source)
-        end
-        obs.source_list_release(sources)
-    end
+    gs = settings
     local sh = obs.obs_get_signal_handler()
     obs.signal_handler_connect(sh, "source_create", source_create)
 end
