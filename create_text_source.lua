3102085700176a792eb30d8d6ab4965a6a72e19d
Exeldro <exelsor@hotmail.com>
2021-11-16 09:55:02 +0100

    update create_text_source.lua add settings

--- create_text_source.lua
+++ create_text_source.lua
@@ -1,17 +1,44 @@
 obs = obslua
 hotkey_id = obs.OBS_INVALID_HOTKEY_ID
-source_base_name = "Text"
-font = nil
+global_settings = nil
 
 function script_properties()
     local props = obs.obs_properties_create()
     obs.obs_properties_add_text(props, "source_name", "Source Name", obs.OBS_TEXT_DEFAULT)
     obs.obs_properties_add_font(props, "font", "Font")
+    obs.obs_properties_add_color(props, "color", "Color")
+    local p = obs.obs_properties_add_list(props, "align", "Align", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
+    obs.obs_property_list_add_string(p, "Left", "left");
+	obs.obs_property_list_add_string(p, "Center", "center");
+	obs.obs_property_list_add_string(p, "Right", "right");
+    p = obs.obs_properties_add_list(props, "valign", "Vertical Align", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
+    obs.obs_property_list_add_string(p, "Top", "top");
+	obs.obs_property_list_add_string(p, "Center", "center");
+	obs.obs_property_list_add_string(p, "Bottom", "bottom");
+    local extents = obs.obs_properties_create()
+    obs.obs_properties_add_int(extents, "extents_cx", "X", 32, 8000, 1);
+	obs.obs_properties_add_int(extents, "extents_cy", "Y", 32, 8000, 1);
+	obs.obs_properties_add_bool(extents, "extents_wrap", "Wrap");
+    obs.obs_properties_add_group(props,"extents", "Extents", obs.OBS_GROUP_CHECKABLE, extents)
+    obs.obs_properties_add_text(props, "text", "Text", obs.OBS_TEXT_MULTILINE)
     return props
 end
 
 function script_defaults(settings)
+    local font = obs.obs_data_create()
+    obs.obs_data_set_default_string(font, "face", "Arial");
+	obs.obs_data_set_default_int(font, "size", 256);
+	obs.obs_data_set_default_obj(settings, "font", font);
+    obs.obs_data_release(font)
+
     obs.obs_data_set_default_string(settings, "source_name", "Text")
+    obs.obs_data_set_default_int(settings, "color", 0xFFFFFF)
+    obs.obs_data_set_default_string(settings, "align", "left")
+    obs.obs_data_set_default_string(settings, "valign", "top")
+
+    obs.obs_data_set_default_bool(settings, "extents_wrap", true);
+	obs.obs_data_set_default_int(settings, "extents_cx", 100);
+	obs.obs_data_set_default_int(settings, "extents_cy", 100);
 end
 
 function script_description()
@@ -19,17 +46,14 @@ function script_description()
 end
 
 function script_update(settings)
-    source_base_name = obs.obs_data_get_string(settings, "source_name")
-    if font ~= nil then
-        obs.obs_data_release(font)
-    end
-    font = obs.obs_data_get_obj(settings, "font")
+    
 end
 
 function create_text_source_trigger(pressed)
 	if not pressed then
 		return
     end
+    local source_base_name = obs.obs_data_get_string(global_settings, "source_name")
     local source_name = source_base_name
     local source = obs.obs_get_source_by_name(source_name)
     local source_count = 1
@@ -40,24 +64,30 @@ function create_text_source_trigger(pressed)
         source = obs.obs_get_source_by_name(source_name)
         source_count = source_count + 1
     end
-    local text_settings = obs.obs_data_create()
-    obs.obs_data_set_obj(text_settings, "font", font)
+
+    local settings = obs.obs_data_create_from_json(obs.obs_data_get_json(global_settings))
     if os.getenv('OS'):lower():match('windows') then
-        source = obs.obs_source_create("text_gdiplus", source_name, text_settings, nil)
+        source = obs.obs_source_create(obs.obs_get_latest_input_type_id("text_gdiplus"), source_name, settings, nil)
     else
-        source = obs.obs_source_create("text_ft2_source", source_name, text_settings, nil)
+        source = obs.obs_source_create(obs.obs_get_latest_input_type_id("text_ft2_source"), source_name, settings, nil)
     end
-    obs.obs_data_release(text_settings)
+    obs.obs_data_release(settings)
     if source ~= nil then
         local scene_source = obs.obs_frontend_get_current_scene()
         local scene = obs.obs_scene_from_source(scene_source)
         local item = obs.obs_scene_add(scene, source)
+        local pos = obs.vec2()
+        pos.x = obs.obs_source_get_width(scene_source)/2
+        pos.y = obs.obs_source_get_height(scene_source)/2
+        obs.obs_sceneitem_set_pos(item, pos)
+        obs.obs_sceneitem_set_alignment(item, 0)
         obs.obs_source_release(scene_source)
         obs.obs_source_release(source)
     end
 end
 
 function script_load(settings)
+    global_settings = settings
     hotkey_id = obs.obs_hotkey_register_frontend("create_text_source.trigger","Add Text Source", create_text_source_trigger)
     local hotkey_save_array = obs.obs_data_get_array(settings, "create_text_source.trigger")
 	obs.obs_hotkey_load(hotkey_id, hotkey_save_array)
@@ -71,8 +101,5 @@ function script_save(settings)
 end
 
 function script_unload()
-    if font ~= nil then
-        obs.obs_data_release(font)
-        font = nil
-    end
+
 end
